d65659fc6955bfe9dd5af96ee1e64da7dd4fc9e1
Benji Hooper <benji@sixp.in>
2018-09-19 17:59:55 +0100

    makes errand use inputs of all types

--- my-errand.sh
+++ my-errand.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -e
-echo "this is an errand"
+echo "This is an Errand running inputs!"
+echo "Hello string input!  ${INPUT_string_input}!"
+echo "Hello list input! ${INPUT_list_input}!"
+echo "Hello integer input! ${INPUT_integer_input}!"
+echo "Hello bool input! ${INPUT_bool_input}!"
